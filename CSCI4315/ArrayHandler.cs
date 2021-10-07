9bb2795af6d3ae08ba14ba75e2915251f380ebb5
Vu Nguyen <vu-nguyenhoang1995@users.noreply.github.com>
2021-10-07 01:03:31 -0500

    first push

---
+++ CSCI4315/ArrayHandler.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace CSCI4315
+{
+    public static class ArrayHandler
+    {
+        public static double Average(int sum, int length)
+        {
+            return sum/length;
+        }
+
+        public static double Average(int[] _array){
+            int sum = 0;
+            foreach(var i in _array){
+                sum += i;
+            }
+            return sum/_array.Length;
+        }
+
+        public static int Sum(int[] _array)
+        {
+            int sum = 0;
+            foreach(var i in _array){
+                sum += i;
+            }
+
+            return sum;
+        }
+
+        public static int LargestNumber(int[] _array){
+            int largest = (from i in _array orderby i descending select i).FirstOrDefault();
+
+            // foreach(var i in largest){
+            //     Console.Write($"{i}\t");
+            // }
+            // Console.WriteLine();
+            //Console.WriteLine($"The largest number is {largest}");
+
+            return largest;
+        }
+
+        public static int SecondLargest(int[] _array){
+            int secondLargest = (from i in _array orderby i descending select i).ElementAtOrDefault(1);
+
+            return secondLargest;
+        }
+
+        public static double StandardDeviation(int[] _array){
+            double standardDeviation = 0;
+            double sampleMean = ((int)_array.Average()) / _array.Length;
+
+            double t = 0;
+            foreach(int i in _array){
+                t += Math.Pow(i - sampleMean,2);
+            }
+            standardDeviation = Math.Sqrt(t/(_array.Length -1));
+            Console.WriteLine($"sample mean: {sampleMean} and \nstandard deviation: {standardDeviation}");
+
+            return 0;
+        }
+    }
+}
--- CSCI4315/Program.cs
+++ CSCI4315/Program.cs
@@ -6,7 +6,31 @@ namespace CSCI4315
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random rd = new Random();
+            int[] array = new int[10];
+
+            for(int i = 0; i < array.Length; i++){
+                array[i]= rd.Next(1,100);
+            }
+
+            //display int array
+            for(int i = 0; i < array.Length; i++){
+                Console.Write(array[i]+ "\t");
+            }
+            Console.WriteLine();
+            int sum = ArrayHandler.Sum(array);
+            Console.WriteLine($"Sum = {sum}");
+
+            double avg = ArrayHandler.Average(sum, array.Length);
+            Console.WriteLine($"Average = {avg}");
+
+            int theLargest = ArrayHandler.LargestNumber(array);
+            Console.WriteLine($"The largest = {theLargest}");
+
+            int theSecondLargest = ArrayHandler.SecondLargest(array);
+            Console.WriteLine($"The second largest = {theSecondLargest}");
+
+            ArrayHandler.StandardDeviation(array);
         }
     }
 }
